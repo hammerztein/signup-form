099d7048c2ac3006088cbcf9041b02706f1ab1ca
Aleksandr <113410543+hammerztein@users.noreply.github.com>
2024-06-18 09:32:55 +0300

    Add validation class on button click. Previously when trying to submit form invalid inputs didnt get the proper class.

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
+const submitBtn = document.querySelector('form button[type="submit"]');
 const inputFields = document.querySelectorAll('input:not([type="checkbox"');
 
+// Function to add validate class
 function addValidateClass(e) {
 	const node = e.target;
 	node.classList.add('validate');
 }
 
+// Input field event listener
 inputFields.forEach((input) => {
 	input.addEventListener('blur', addValidateClass);
+});
+
+// Button event listener
+submitBtn.addEventListener('click', () => {
+	inputFields.forEach((input) => input.classList.add('validate'));
 });
